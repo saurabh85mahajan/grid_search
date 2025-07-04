7c22ed99ba31565e71eb0a87d99401830bbeb546
AscSoftwares <support@ascsoftwares.com>
2025-07-04 17:30:58 +0530

    Update button label

--- app/Filament/Llc/Resources/CcResource/Pages/CreateCc.php
+++ app/Filament/Llc/Resources/CcResource/Pages/CreateCc.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Llc\Resources\CcResource\Pages;
 
 use App\Filament\Llc\Resources\CcResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCc extends CreateRecord
@@ -12,5 +13,19 @@ class CreateCc extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction(),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    protected function getCreateFormAction(): Action
+    {
+        return parent::getCreateFormAction()
+            ->label('Save');
     }
 }
--- app/Filament/Resources/InsuranceTypeResource/Pages/CreateInsuranceType.php
+++ app/Filament/Resources/InsuranceTypeResource/Pages/CreateInsuranceType.php
@@ -4,9 +4,24 @@ namespace App\Filament\Resources\InsuranceTypeResource\Pages;
 
 use App\Filament\Resources\InsuranceTypeResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateInsuranceType extends CreateRecord
 {
     protected static string $resource = InsuranceTypeResource::class;
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction(),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    protected function getCreateFormAction(): Action
+    {
+        return parent::getCreateFormAction()
+            ->label('Save');
+    }
 }
