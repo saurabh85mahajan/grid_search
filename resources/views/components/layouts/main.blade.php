d48a1f63f2aa215d2c34061640c373392b7812e4
AscSoftwares <support@ascsoftwares.com>
2025-11-26 12:08:35 +0530

    GA Added

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Insurance Policy Search' }}</title>
     
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TQKBX66Z');</script>
+    <!-- End Google Tag Manager -->
+
     @filamentStyles
     @vite('resources/css/filament/admin/theme.css')
     
@@ -30,6 +38,10 @@
     </style>
 </head>
 <body class="antialiased fi-body min-h-full bg-gray-50">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQKBX66Z"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- Simple Navigation Bar -->
     <nav class="nav-bar shadow-sm">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
